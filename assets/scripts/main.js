7fc96dcaf6167ecd114d89b0a2f17f65db33e4f5
Manav Ram <mram@ucsd.edu>
2021-02-05 18:26:40 -0800

    testing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,7 @@
 
 var audioValue = document.getElementById("volume-number");
 var audio = document.getElementById("horn-sound");
-audio.volume = audioValue;
+console.log(audioValue);
 var clicked = document.getElementById("honk-btn-container").addEventListener("click",function(){ alert("Testing"); });
 
 
