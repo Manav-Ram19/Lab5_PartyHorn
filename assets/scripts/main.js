072c6c0b5182691b6235694cf9098676cb125e88
Manav Ram <mram@ucsd.edu>
2021-02-05 19:30:07 -0800

    audio working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,10 +2,13 @@
 
 // TODO
 
-var audioValue = document.getElementById("volume-number");
 var audio = document.getElementById("horn-sound");
-console.log(audioValue);
-var clicked = document.getElementById("honk-btn-container").addEventListener("click",function(){ alert("Testing"); });
+var audioValue = document.getElementById("volume-number").value;
+var formData = document.getElementById("party-horn-form");
+formData.addEventListener("submit", mainFunc);
 
-
-
+function mainFunc(event) {
+    alert(audioValue);
+    event.preventDefault();
+    audio.play();
+}
